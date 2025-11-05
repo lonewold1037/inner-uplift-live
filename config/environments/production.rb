b4a0eee3e4efdd3863f42d40713651dd3cba8d44
Lonewolf1037 <ryancmcleod@gmail.com>
2025-11-05 01:55:59 +0000

    Rebrand to Memflect and add custom domain config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,13 +81,14 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
-  #
+  config.hosts = [
+    "memflect.com",
+    "www.memflect.com",
+    "inner-uplift.onrender.com"
+  ]
+  
   # Skip DNS rebinding protection for the default health check endpoint.
-  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
   # ActionCable configuration for WebSocket connections
   config.action_cable.url = "wss://inner-uplift.onrender.com/cable"
