c2e6645d6044bc4c0c00b2225b382f5689704ea2
Lonewolf1037 <ryancmcleod@gmail.com>
2025-11-06 09:10:39 +0000

    more def change verbiage to go with disable ddl transaction migrate facilitate

--- db/migrate/20251106041525_add_purchase_fields_to_reflections.rb
+++ db/migrate/20251106041525_add_purchase_fields_to_reflections.rb
@@ -2,8 +2,8 @@ class AddPurchaseFieldsToReflections < ActiveRecord::Migration[7.2]
   disable_ddl_transaction!
   
   def change
-    add_column :reflections, :purchased, :boolean
+    add_column :reflections, :purchased, :boolean, default: false, null: false
     add_column :reflections, :extended_recap, :text
-    add_index :reflections, :purchased
+    add_index :reflections, :purchased, algorithm: :concurrently
   end
 end
