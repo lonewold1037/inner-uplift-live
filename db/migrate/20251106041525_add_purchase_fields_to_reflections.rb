ab79de466e105f79d424ddce249eb43a9230e362
Lonewolf1037 <ryancmcleod@gmail.com>
2025-11-06 09:08:55 +0000

    disable_ddl_transaction! added to new migration to migrate in render

--- db/migrate/20251106041525_add_purchase_fields_to_reflections.rb
+++ db/migrate/20251106041525_add_purchase_fields_to_reflections.rb
@@ -1,4 +1,6 @@
 class AddPurchaseFieldsToReflections < ActiveRecord::Migration[7.2]
+  disable_ddl_transaction!
+  
   def change
     add_column :reflections, :purchased, :boolean
     add_column :reflections, :extended_recap, :text
