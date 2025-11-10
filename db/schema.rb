45fe4d9a9804b66cc1cd55d30ffd0962c067dc33
Lonewolf1037 <ryancmcleod@gmail.com>
2025-11-10 02:42:35 +0000

    Skip CSRF verification for guest checkout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_11_06_041525) do
+ActiveRecord::Schema[7.2].define(version: 2025_11_07_001343) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -151,6 +151,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_06_041525) do
     t.string "status"
     t.boolean "purchased"
     t.text "extended_recap"
+    t.string "title"
     t.index ["soundscape_id"], name: "index_reflections_on_soundscape_id"
     t.index ["status"], name: "index_reflections_on_status"
     t.index ["user_id"], name: "index_reflections_on_user_id"
