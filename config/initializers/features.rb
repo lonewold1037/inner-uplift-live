aa16a6365e2550dd9084af1ea515c068e973e40f
Lonewolf1037 <ryancmcleod@gmail.com>
2026-01-12 00:56:31 +0000

    turning off STT in features.rb

--- config/initializers/features.rb
+++ config/initializers/features.rb
@@ -4,7 +4,7 @@ module Features
   # When enabled, users can adjust voice/music balance before purchase
   EQ_PRESETS_ENABLED = false
 
-  SPEECH_TO_TEXT_ENABLED = true
+  SPEECH_TO_TEXT_ENABLED = false
   
   # Future feature flags can go here:
   # MULTI_LANGUAGE_ENABLED = false
