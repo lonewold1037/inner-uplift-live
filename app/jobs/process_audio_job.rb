9711a6ca55cdb8a35b5a0b5dc9d8526207ecd2f9
Lonewolf1037 <ryancmcleod@gmail.com>
2026-03-02 01:46:35 +0000

    trying .30 stabilit on alpha3 11labs

--- app/jobs/process_audio_job.rb
+++ app/jobs/process_audio_job.rb
@@ -129,7 +129,7 @@ class ProcessAudioJob < ApplicationJob
     uri = URI.parse("https://api.elevenlabs.io/v1/voices/#{voice_id}/settings/edit")
     headers = { "Content-Type" => "application/json", "xi-api-key" => ENV["ELEVEN_LABS_API_KEY"] }
     body = {
-      stability: 0.50
+      stability: 0.30
     }.to_json
     Net::HTTP.post(uri, body, headers)
   end
