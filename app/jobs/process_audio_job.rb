4c6bbf4a39976a70509fe9c86dc86d2faf309d57
Lonewolf1037 <ryancmcleod@gmail.com>
2026-03-02 06:38:03 +0000

    .81 .91 .91 text (.75x2 was not me haha)

--- app/jobs/process_audio_job.rb
+++ app/jobs/process_audio_job.rb
@@ -129,10 +129,10 @@ class ProcessAudioJob < ApplicationJob
     uri = URI.parse("https://api.elevenlabs.io/v1/voices/#{voice_id}/settings/edit")
     headers = { "Content-Type" => "application/json", "xi-api-key" => ENV["ELEVEN_LABS_API_KEY"] }
     body = {
-      stability: 0.75,
-      similarity_boost: 0.75,
+      stability: 0.81,
+      similarity_boost: 0.91,
       use_speaker_boost: true,
-      speed: 0.92
+      speed: 0.91
     }.to_json
     Net::HTTP.post(uri, body, headers)
   end
