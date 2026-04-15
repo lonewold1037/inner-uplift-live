f190acc4cee823722ba23b27b3bfb7cf85ce994f
Lonewolf1037 <ryancmcleod@gmail.com>
2026-04-15 01:14:29 +0000

    Add memcard fields to reflections

---
+++ db/migrate/20260413000000_add_memcard_fields_to_reflections.rb
@@ -0,0 +1,10 @@
+class AddMemcardFieldsToReflections < ActiveRecord::Migration[7.2]
+  def change
+    add_column :reflections, :memcard_token, :string
+    add_column :reflections, :memcard_enabled, :boolean, default: false, null: false
+    add_column :reflections, :memcard_view_count, :integer, default: 0, null: false
+    add_column :reflections, :memcard_enabled_at, :datetime
+
+    add_index :reflections, :memcard_token, unique: true
+  end
+end
